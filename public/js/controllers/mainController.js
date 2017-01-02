e499394478858d41602242cd877d0d17a47b9d35
medaak <medaakdev@gmail.com>
2017-01-02 15:04:47 +0100

    edit mainpage and maincontroller

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,31 +1,35 @@
-function mainController(todoService) {
+import randomFR from 'random-word-fr';
 
-    this.todoService = todoService;
+function mainController() {
 
-    this.load = () => {
-        this.todoService.getAll().then((res) => {
-            this.todos = res.data;
-        });
-    };
-
-    this.create = () => {
-        this.todoService.create(this.todo).then(() => {
-            this.todo = '';
-            this.load();
-        });
-    };
-
-    this.update = (todo) => {
-        this.todoService.update(todo._id, todo.description).then(() => {
-            this.load();
-        });
-    };
-
-    this.delete = (todo) => {
-        this.todoService.delete(todo._id).then(() => {
-            this.load();
-        });
-    };
-
-    this.load();
+    var wordToFind = randomFR();
+    console.log(wordToFind);
+    // this.todoService = todoService;
+    //
+    // this.load = () => {
+    //     this.todoService.getAll().then((res) => {
+    //         this.todos = res.data;
+    //     });
+    // };
+    //
+    // this.create = () => {
+    //     this.todoService.create(this.todo).then(() => {
+    //         this.todo = '';
+    //         this.load();
+    //     });
+    // };
+    //
+    // this.update = (todo) => {
+    //     this.todoService.update(todo._id, todo.description).then(() => {
+    //         this.load();
+    //     });
+    // };
+    //
+    // this.delete = (todo) => {
+    //     this.todoService.delete(todo._id).then(() => {
+    //         this.load();
+    //     });
+    // };
+    //
+    // this.load();
 }
